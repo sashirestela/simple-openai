f963bc141166e926d1000784cbc68006a3ae7ec8
Sashir Estela <sashirestela@yahoo.com>
2023-08-21 03:38:59 +0000

    Fixing minor issue with ModerationRequest

--- src/main/java/io/github/sashirestela/openai/domain/moderation/ModerationRequest.java
+++ src/main/java/io/github/sashirestela/openai/domain/moderation/ModerationRequest.java
@@ -2,6 +2,9 @@ package io.github.sashirestela.openai.domain.moderation;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,9 +18,11 @@ import lombok.ToString;
 @ToString
 @Builder
 public class ModerationRequest {
-  
+
   @NonNull
   private List<String> input;
 
+  @JsonInclude(Include.NON_NULL)
   private String model;
+
 }
