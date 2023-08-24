7a4785c0e918410f8e19293e9e1543a25490902e
Sashir Estela <sashirestela@yahoo.com>
2023-08-24 04:17:28 +0000

    Adding @Documented to custom annotations

--- src/main/java/io/github/sashirestela/openai/http/annotation/DELETE.java
+++ src/main/java/io/github/sashirestela/openai/http/annotation/DELETE.java
@@ -1,10 +1,12 @@
 package io.github.sashirestela.openai.http.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface DELETE {
