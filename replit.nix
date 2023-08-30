d9b0089ea405fee5e537d5a3bff9df21d78a99fa
sashirestela <sashirestela@gmail.com>
2023-08-30 22:10:51 +0000

    Configure Gpg

--- replit.nix
+++ replit.nix
@@ -1,9 +1,19 @@
 { pkgs }: {
     deps = [
+        pkgs.nvi
+        pkgs.gnupg
+        pkgs.pinentry
+        pkgs.pinentry-curses
         pkgs.zip
         pkgs.graalvm11-ce
         pkgs.maven
         pkgs.replitPackages.jdt-language-server
         pkgs.replitPackages.java-debug
     ];
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+     enable = true;
+     pinentryFlavor = "curses";
+     enableSSHSupport = true;
+  };
 }
