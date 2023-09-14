9e6846a4e921f4e5fe66ac65839542492fc4201b
Sashir Estela <sashirestela@yahoo.com>
2023-09-14 02:27:26 +0000

    Refactoring Lombok annotations to Model domain

--- src/main/java/io/github/sashirestela/openai/domain/moderation/ModerationRequest.java
+++ src/main/java/io/github/sashirestela/openai/domain/moderation/ModerationRequest.java
@@ -5,17 +5,11 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.ToString;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
-@ToString
 @Builder
 public class ModerationRequest {
 
