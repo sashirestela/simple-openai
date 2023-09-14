78483a3d2d5ff637e06ecd76701fd0006474e73d
Sashir Estela <sashirestela@yahoo.com>
2023-09-14 03:10:28 +0000

    Refactoring Lombok annotations to OpenAI domain

--- src/main/java/io/github/sashirestela/openai/domain/OpenAIGeneric.java
+++ src/main/java/io/github/sashirestela/openai/domain/OpenAIGeneric.java
@@ -7,9 +7,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 @Getter
 public class OpenAIGeneric<T> {
 
