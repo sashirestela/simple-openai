22f4e9ffeba2f424ced910fbdb4c44087ec246a2
Sashir Estela <sashirestela@yahoo.com>
2023-09-14 03:28:52 +0000

    Fixing testing coverage

--- src/main/java/io/github/sashirestela/openai/domain/file/FileRequest.java
+++ src/main/java/io/github/sashirestela/openai/domain/file/FileRequest.java
@@ -2,12 +2,10 @@ package io.github.sashirestela.openai.domain.file;
 
 import java.nio.file.Path;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 
-@AllArgsConstructor
 @Getter
 @Builder
 public class FileRequest {
