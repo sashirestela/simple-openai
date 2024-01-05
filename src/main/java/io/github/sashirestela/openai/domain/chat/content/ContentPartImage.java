6f3c94bd149b8fa3062bc359613784d27f0c533f
Sashir Estela <sashirestela@yahoo.com>
2024-01-05 16:10:41 +0000

    Fixing issue with ContentPartImage

--- src/main/java/io/github/sashirestela/openai/domain/chat/content/ContentPartImage.java
+++ src/main/java/io/github/sashirestela/openai/domain/chat/content/ContentPartImage.java
@@ -1,11 +1,14 @@
 package io.github.sashirestela.openai.domain.chat.content;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.NonNull;
 
 @Getter
 public class ContentPartImage extends ContentPart {
 
+    @JsonProperty("image_url")
     private ImageUrl imageUrl;
 
     public ContentPartImage(@NonNull ImageUrl imageUrl) {
