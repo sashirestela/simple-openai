ad754547a531a923aae92ee68363edf8af683ff3
Sashir Estela <sashirestela@yahoo.com>
2024-02-19 16:42:33 +0000

    Fix to generate json schema without depending from flelds

--- src/main/java/io/github/sashirestela/openai/support/JsonSchemaUtil.java
+++ src/main/java/io/github/sashirestela/openai/support/JsonSchemaUtil.java
@@ -1,6 +1,5 @@
 package io.github.sashirestela.openai.support;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.victools.jsonschema.generator.Option;
@@ -10,6 +9,7 @@ import com.github.victools.jsonschema.generator.SchemaGeneratorConfigBuilder;
 import com.github.victools.jsonschema.generator.SchemaVersion;
 import com.github.victools.jsonschema.module.jackson.JacksonModule;
 import com.github.victools.jsonschema.module.jackson.JacksonOption;
+
 import io.github.sashirestela.openai.SimpleUncheckedException;
 
 public class JsonSchemaUtil {
@@ -22,28 +22,22 @@ public class JsonSchemaUtil {
 
     public static JsonNode classToJsonSchema(Class<?> clazz) {
         JsonNode jsonSchema = null;
-        if (clazz.getFields().length > 0) {
-            try {
-                var jacksonModule = new JacksonModule(JacksonOption.RESPECT_JSONPROPERTY_REQUIRED);
-                var configBuilder = new SchemaGeneratorConfigBuilder(SchemaVersion.DRAFT_2020_12,
-                        OptionPreset.PLAIN_JSON)
-                        .with(jacksonModule)
-                        .without(Option.SCHEMA_VERSION_INDICATOR);
-                var config = configBuilder.build();
-                var generator = new SchemaGenerator(config);
-                jsonSchema = generator.generateSchema(clazz);
-
-            } catch (Exception e) {
-                throw new SimpleUncheckedException("Cannot generate the Json Schema for the class {0}.",
-                        clazz.getName(), e);
-            }
-        } else {
-            try {
+        try {
+            var jacksonModule = new JacksonModule(JacksonOption.RESPECT_JSONPROPERTY_REQUIRED);
+            var configBuilder = new SchemaGeneratorConfigBuilder(SchemaVersion.DRAFT_2020_12,
+                    OptionPreset.PLAIN_JSON)
+                    .with(jacksonModule)
+                    .without(Option.SCHEMA_VERSION_INDICATOR);
+            var config = configBuilder.build();
+            var generator = new SchemaGenerator(config);
+            jsonSchema = generator.generateSchema(clazz);
+            if (jsonSchema.get("properties") == null) {
                 jsonSchema = objectMapper.readTree(JSON_EMPTY_CLASS);
-            } catch (JsonProcessingException e) {
-                throw new SimpleUncheckedException("Cannot generate the Json Schema for the class {0}.",
-                        clazz.getName(), e);
             }
+
+        } catch (Exception e) {
+            throw new SimpleUncheckedException("Cannot generate the Json Schema for the class {0}.",
+                    clazz.getName(), e);
         }
         return jsonSchema;
     }
