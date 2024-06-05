5d06303cd979f235af8414421f2eafe931d96499
Sashir Estela <sashirestela@gmail.com>
2024-06-05 05:05:21 +0000

    Support for File Search Customizations

--- src/main/java/io/github/sashirestela/openai/domain/assistant/AssistantTool.java
+++ src/main/java/io/github/sashirestela/openai/domain/assistant/AssistantTool.java
@@ -1,8 +1,11 @@
 package io.github.sashirestela.openai.domain.assistant;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.github.sashirestela.openai.common.tool.Tool;
 import io.github.sashirestela.openai.common.tool.ToolType;
+import io.github.sashirestela.slimvalidator.constraints.Range;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -11,13 +14,42 @@ import lombok.ToString;
 @Getter
 @ToString
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AssistantTool extends Tool {
 
-    public static final AssistantTool CODE_INTERPRETER = new AssistantTool(ToolType.CODE_INTERPRETER, null);
-    public static final AssistantTool FILE_SEARCH = new AssistantTool(ToolType.FILE_SEARCH, null);
+    private FileSearch fileSearch;
 
-    private AssistantTool(ToolType type, ToolFunctionDef function) {
-        super(type, function);
+    private AssistantTool(ToolType type, FileSearch fileSearch) {
+        super(type, null);
+        this.fileSearch = fileSearch;
+    }
+
+    public static AssistantTool codeInterpreter() {
+        return new AssistantTool(ToolType.CODE_INTERPRETER, null);
+    }
+
+    public static AssistantTool fileSearch() {
+        return new AssistantTool(ToolType.FILE_SEARCH, null);
+    }
+
+    public static AssistantTool fileSearch(Integer maxNumResults) {
+        return new AssistantTool(ToolType.FILE_SEARCH, new FileSearch(maxNumResults));
+    }
+
+    @NoArgsConstructor
+    @Getter
+    @ToString
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    public static class FileSearch {
+
+        @Range(min = 1, max = 50)
+        private Integer maxNumResults;
+
+        public FileSearch(Integer maxNumResults) {
+            this.maxNumResults = maxNumResults;
+        }
+
     }
 
 }
