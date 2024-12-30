0b135897c06b0e40f93513df6fc4862227115fab
Sashir Estela <sashirestela@gmail.com>
2024-12-30 18:54:12 +0000

    Rename unit test file

--- src/test/java/io/github/sashirestela/openai/SimpleOpenAIExceptionTest.java
+++ src/test/java/io/github/sashirestela/openai/SimpleOpenAIExceptionTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class SimpleUncheckedExceptionTest {
+class SimpleOpenAIExceptionTest {
 
     @Test
     void shouldSetMessageWhenItIsPassedAsTheOnlyOneArgument() {
