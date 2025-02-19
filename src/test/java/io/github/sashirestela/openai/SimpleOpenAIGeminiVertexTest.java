13bd770b0aefa22087c7b8addc977545e3db61a7
the-gigi <the.gigi@gmail.com>
2025-02-18 23:08:06 -0800

    make test class non public

--- src/test/java/io/github/sashirestela/openai/SimpleOpenAIGeminiVertexTest.java
+++ src/test/java/io/github/sashirestela/openai/SimpleOpenAIGeminiVertexTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class SimpleOpenAIGeminiVertexTest {
+class SimpleOpenAIGeminiVertexTest {
 
     UnaryOperator<HttpResponseData> responseInterceptor;
 
