5eed6e09fe175fbde789f66777886fe8ae1c18f6
Sashir Estela <sashirestela@gmail.com>
2025-02-27 12:45:27 -0500

    Support for updates in Modeation API

--- src/demo/java/io/github/sashirestela/openai/demo/ModerationDemo.java
+++ src/demo/java/io/github/sashirestela/openai/demo/ModerationDemo.java
@@ -1,6 +1,8 @@
 package io.github.sashirestela.openai.demo;
 
 import io.github.sashirestela.openai.domain.moderation.ModerationRequest;
+import io.github.sashirestela.openai.domain.moderation.ModerationRequest.MultiModalInput.ImageUrlInput;
+import io.github.sashirestela.openai.domain.moderation.ModerationRequest.MultiModalInput.TextInput;
 
 import java.util.Arrays;
 
@@ -9,8 +11,10 @@ public class ModerationDemo extends AbstractDemo {
     public void demoCallModerationCreate() {
         var moderationRequest = ModerationRequest.builder()
                 .input(Arrays.asList(
-                        "I want to kill them.",
-                        "I am not sure what to think about them."))
+                        TextInput.of("I want to kill them."),
+                        ImageUrlInput.of("https://upload.wikimedia.org/wikipedia/commons/e/e3/BWHammerSickle.jpg"),
+                        TextInput.of("I am not sure what to think about them.")))
+                .model("omni-moderation-latest")
                 .build();
         var futureModeration = openAI.moderations().create(moderationRequest);
         var moderationResponse = futureModeration.join();
--- src/main/java/io/github/sashirestela/openai/domain/moderation/ModerationRequest.java
+++ src/main/java/io/github/sashirestela/openai/domain/moderation/ModerationRequest.java
@@ -2,11 +2,15 @@ package io.github.sashirestela.openai.domain.moderation;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.github.sashirestela.slimvalidator.constraints.ObjectType;
 import io.github.sashirestela.slimvalidator.constraints.Required;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.List;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Builder
@@ -14,8 +18,82 @@ import java.util.List;
 public class ModerationRequest {
 
     @Required
-    private List<String> input;
+    @ObjectType(baseClass = String.class)
+    @ObjectType(baseClass = String.class, firstGroup = true)
+    @ObjectType(baseClass = MultiModalInput.class, firstGroup = true)
+    private Object input;
 
     private String model;
+
+    public enum MultiModalType {
+
+        @JsonProperty("text")
+        TEXT,
+
+        @JsonProperty("image_url")
+        IMAGE_URL;
+
+    }
+
+    @Getter
+    @Setter
+    public abstract static class MultiModalInput {
+
+        protected MultiModalType type;
+
+        @Getter
+        @ToString
+        @JsonInclude(Include.NON_EMPTY)
+        @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+        public static class TextInput extends MultiModalInput {
+
+            @Required
+            private String text;
+
+            private TextInput(String text) {
+                this.type = MultiModalType.TEXT;
+                this.text = text;
+            }
+
+            public static TextInput of(String text) {
+                return new TextInput(text);
+            }
+
+        }
+
+        @Getter
+        @ToString
+        @JsonInclude(Include.NON_EMPTY)
+        @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+        public static class ImageUrlInput extends MultiModalInput {
+
+            @Required
+            private ImageUrl imageUrl;
+
+            private ImageUrlInput(ImageUrl imageUrl) {
+                this.type = MultiModalType.IMAGE_URL;
+                this.imageUrl = imageUrl;
+            }
+
+            public static ImageUrlInput of(String url) {
+                return new ImageUrlInput(new ImageUrl(url));
+            }
+
+            @Getter
+            @ToString
+            public static class ImageUrl {
+
+                @Required
+                private String url;
+
+                public ImageUrl(String url) {
+                    this.url = url;
+                }
+
+            }
+
+        }
+
+    }
 
 }
